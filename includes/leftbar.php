9d0a25234d2011f4c62b7ab10cfee52be139a140
NamAnh <tnnamanh@gmail.com>
2020-06-30 18:39:23 +0700

    Init

---
+++ includes/leftbar.php
@@ -0,0 +1,17 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label">Main</li>
+			<li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Thông tin cá nhân</a>
+			</li>
+			<li><a href="feedback.php"><i class="fa fa-envelope"></i> &nbsp;Phản hồi</a>
+			</li>
+			<li><a href="notification.php"><i class="fa fa-bell"></i> &nbsp;Thông báo<sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="messages.php"><i class="fa fa-envelope"></i> &nbsp;Tin nhắn</a>
+			</li>
+			</ul>
+			<p class="text-center" style="color:#ffffff; margin-top: 100px;">All right served</p>
+		</nav>
+
+		
